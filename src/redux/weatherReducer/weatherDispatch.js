a631d8eefaca735891ebf0c8e3ede26cdeae1c0b
Ehsan <ehsan.aryan2001@gmail.com>
2025-05-14 09:19:59 +0330

    Edit API call.

--- src/redux/weatherReducer/weatherDispatch.js
+++ src/redux/weatherReducer/weatherDispatch.js
@@ -3,7 +3,7 @@ import axios from "axios"
 export const handleWeatherAPI = (query) => {
     return (dispatch) => {
         dispatch({type : "send-weather-request"});
-        axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${query}&appid=c2fe170db43574838e6adf54622988b9`)
+        axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${query}&appid=${process.env.REACT_APP_API_KEY}`)
         .then(response => {
             dispatch({type : "get-weather-response" , payload : response.data});
         })
@@ -16,7 +16,7 @@ export const handleWeatherAPI = (query) => {
 export const handleWeatherAPILatLon = (lat , lon) => {
     return (dispatch) => {
         dispatch({type : "send-weather-request"});
-        axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=c2fe170db43574838e6adf54622988b9`)
+        axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${process.env.REACT_APP_API_KEY}`)
         .then(response => {
             dispatch({type : "get-weather-response" , payload : response.data});
         })
